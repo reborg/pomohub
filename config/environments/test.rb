e559b45d92513d8d820574174dd9cb364c77b7c3
reborg <reborg@reborg.net>
2009-06-14 18:22:00 -0500

    all the haml sass compass blueprint plumbing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,8 +27,7 @@ config.action_mailer.delivery_method = :test
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
 
-config.gem 'notahat-machinist', 
-  :version => ">= 0.3.1", 
-  :source => "http://gems.github.com", 
-  :lib => "machinist"
+config.gem 'notahat-machinist', :version => ">= 0.3.1", :source => "http://gems.github.com", :lib => "machinist"
 config.gem "faker"
+config.gem "rspec", :lib => false, :version => ">= 1.2.0"
+config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
